26e7c94773fc6668a6dd2e7935d4f22afe2ada1d
Xlnzero <62247456+Xlnzero@users.noreply.github.com>
2024-02-01 10:29:20 +0300

    wrhww56w546

--- pamyt/main/views.py
+++ pamyt/main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    date = {
+        'title': 'Главная страница!!!',
+        'values': ['Some','Hello','123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Football'
+
+
+        }
+    }
+    return render(request, 'main/index.html', date)
 
 def about(request):
     return render(request, 'main/about.html')
--- pamyt/pamyt/settings.py
+++ pamyt/pamyt/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'main',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
